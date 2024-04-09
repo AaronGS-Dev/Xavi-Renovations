fb453772772cefa07004294f583fe76d3597201c
DomPar <djps94@outlook.com>
2024-04-09 11:32:08 +0100

    header, outlet, eso

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,21 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='w-full h-1/6 border-4 border-black flex'>
-        <div className='h-2/5 text-red-500'>Header</div>
-        <div className='h-3/5 w-full border border-black'>
-
+    <div className='w-full h-1/6 border-4 border-black flex flex-col'>
+        <div className='h-3/5 flex justify-center items-center'>
+            <div className='flex flex-col items-center justify-center'>
+                <h1 className='text-4xl'>XaviRenovaties</h1>
+                <h2>Reformas generales</h2>
+            </div>
+            <img src="../public/image/logo2.png" className='h-full' />
+            
+        </div>
+        <div className='h-2/5 w-full border border-black flex justify-center gap-5'>
+            <button>Página principal</button>
+            <button>Nuestros trabajos</button>
+            <button>Sobre nosotros</button>
+            <button>Nuestro proceso</button>
+            <button>Contáctanos</button>
         </div>
     </div>
   )
