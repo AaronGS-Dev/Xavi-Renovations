795ce24fcb3b8a0b0901e14605a578c2071fee6b
Aaron <ravycrow@gmail.com>
2024-04-09 17:04:27 +0100

    home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+// import React, { useState } from 'react';
 
+// const Home = () => {
+//   const [currentDiv, setCurrentDiv] = useState(1);
+ 
+//   const handleClick = (divNumber) => {
+//     setCurrentDiv(divNumber);
+//   };
+
+//   return (
+//     <main className="h-screen overflow-y-auto">
+//       <div id='home1' style={{ display: currentDiv === 1 ? 'block' : 'none' }}>
+//         <div className="h-screen bg-blue-200 flex flex-col justify-center items-center">
+//           <p>Contenido del Home 1</p>
+//           <button className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleClick(2)}>Ir al Home 2</button>
+//         </div>
+//       </div>
+
+//       <div id='home2' style={{ display: currentDiv === 2 ? 'block' : 'none' }}>
+//         <div className="h-screen bg-green-200 flex flex-col justify-center items-center">
+//           <p>Contenido del Home 2</p>
+//           <button className="ml-4 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleClick(1)}>Ir al Home 1</button>
+//           <button className="ml-4 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleClick(3)}>Ir al Home 3</button>
+//        </div>
+//       </div>
+
+//       <div id='home3' style={{ display: currentDiv === 3 ? 'block' : 'none' }}>
+//         <div className="h-screen bg-yellow-200 flex flex-col justify-center items-center">
+//           <p>Contenido del Home 3</p>
+//           <button className="ml-4 bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleClick(2)}>Ir al Home 2</button>
+//         </div>
+//       </div>
+//     </main>
+//   );
+// };
+
+// export default Home
+
+import React, { useState } from 'react';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 const Home = () => {
-  return (
-    <div></div>
-  )
-}
+  const [currentDiv, setCurrentDiv] = useState(1);
+ 
+  const handleClick = (divId) => {
+    const div = document.getElementById(divId);
+    div.scrollIntoView({ behavior: "smooth" });
+  };
 
-export default Home
+  return (
+    <main className="h-screen overflow-y-auto">
+      <div id='home1'>
+        <div className="h-screen bg-blue-200 flex flex-col justify-center items-center">
+          <p>Contenido del Home 1</p>
+          <button className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleClick('home2')}><KeyboardArrowDownIcon/></button>
+        </div>
+      </div>
+
+      <div id='home2' >
+        <div className="h-screen bg-green-200 flex flex-col justify-center items-center">
+          <p>Contenido del Home 2</p>
+          <button className="ml-4 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleClick('home1')}><KeyboardArrowUpIcon/></button>
+          <button className="ml-4 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleClick('home3')}><KeyboardArrowDownIcon/></button>
+       </div>
+      </div>
+
+      <div id='home3'>
+        <div className="h-screen bg-yellow-200 flex flex-col justify-center items-center">
+          <p>Contenido del Home 3</p>
+          <button className="ml-4 bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleClick('home2')}><KeyboardArrowUpIcon/></button>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
+
+
+
+
+
--- src/routers/index.router.jsx
+++ src/routers/index.router.jsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../pages/app/App";
-
+import Home from "../pages/Home/Home";
 const router = createBrowserRouter([
     {
         path: '/',
         element: <App/>
-    }
+    },
+    
 ])
 
 export default router;
