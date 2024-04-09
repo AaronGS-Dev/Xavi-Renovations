cc3a8b816fae02e956c02997e1836c011554dc29
DomPar <djps94@outlook.com>
2024-04-09 10:54:55 +0100

    pero bueno

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='w-full h-1/5 bg-blue'>
-        <h1 className='text-white'>Header</h1>
+    <div className='w-full h-1/6 border-4 border-black flex'>
+        <div className='h-2/5 text-red-500'>Header</div>
+        <div className='h-3/5 w-full border border-black'>
+
+        </div>
     </div>
   )
 }
