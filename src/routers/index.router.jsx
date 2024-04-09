35747b2649a809823ca77ad3e96c53c67e0e78dc
DomPar <djps94@outlook.com>
2024-04-09 16:34:08 +0100

    nose

--- src/routers/index.router.jsx
+++ src/routers/index.router.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../pages/app/App";
+import App from "../pages/App/App";
+import OurWork from "../pages/OurWorks/OurWork";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <App/>
+        element: <App/>,
+        children: [
+            {
+                path: '/ourwork',
+                element: <OurWork/>
+            }
+        ]
     }
 ])
 
