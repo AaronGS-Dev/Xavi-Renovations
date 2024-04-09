2defbd80a0cfefea52010a84a20f443e414e598b
DomPar <djps94@outlook.com>
2024-04-09 10:35:49 +0100

    header no tw

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-full h-1/5 bg-blue'>
+        <h1 className='text-white'>Header</h1>
+    </div>
+  )
+}
+
+export default Header
--- src/pages/app/App.jsx
+++ src/pages/app/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 
 import './App.css'
+import Header from '../../components/Header/Header'
 
 function App() {
 
   return (
     <>
-      <h1>Aaron mariquita</h1>
+      <Header/>
     </>
   )
 }
