0b1c5d197a54b5eeaa591f158bad5c324c6365c0
Aaron <ravycrow@gmail.com>
2024-04-12 13:04:55 +0100

    welcome funcional

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,23 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import './Home.css'
+
 const Home = () => {
- 
   return (
     <main className='h-full w-full' >
       <div id='containerHome' className="flex flex-col h-full overflow-y-scroll relative">
-        <div className='fixed'>
-        <button className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleClick('home1')}><KeyboardArrowUpIcon/></button>
-        </div>
-
-          <div id='home1' className=" bg-blue-200 flex flex-col items-center ">
+          <div id='home1' className=" bg-blue-200 flex flex-col items-center aling-end">
             <div className=''>
             <a href='#home2'><KeyboardArrowDownIcon/></a>
             </div>
           </div>
-
-
           <div id='home2' className=" bg-green-200 flex flex-col justify-center items-center">
             <div>
               <h1>Nuestros Servicios</h1>
@@ -25,14 +19,12 @@ const Home = () => {
             <a href='#home1'><KeyboardArrowUpIcon/></a>
             <a href='#home3'><KeyboardArrowDownIcon/></a>
         </div>
-
-
           <div id='home3' className=" bg-yellow-200 flex flex-col justify-center items-center">
             <p>Contenido del Home 3</p>
             <a href='#home2'><KeyboardArrowUpIcon/></a>
           </div>
       </div>
-    </main>
+    </main> 
   );
 };
 
