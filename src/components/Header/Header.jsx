2bac0581e1e0e3ec78225c8e9fb4ba60d8131b57
DomPar <djps94@outlook.com>
2024-04-12 12:01:41 +0100

    contact started

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,19 +15,18 @@ const Header = () => {
   return (
     <div className='w-full h-1/6 border-4 border-black flex flex-col'>
         <div className='h-3/5 flex justify-center items-center'>
-            <div className='flex flex-col items-center justify-center'>
+            <div className='flex flex-col items-center justify-center cursor-pointer' onClick={() => handleClick('/')}>
                 <h1 className='text-4xl'>XaviRenovaties</h1>
                 <h2>Reformas generales</h2>
             </div>
-            <img src="../public/image/logo2.png" className='h-full' />
-            
+            <img src="../public/image/logo2.png" className='h-full' />  
         </div>
         <div className='h-2/5 w-full border border-black flex justify-center gap-5'>
             <button className={activeLink === '/' ? 'font-bold' : ''} onClick={() => handleClick('/')}>Página principal</button>
             <button className={activeLink === '/ourwork' ? 'font-bold' : ''} onClick={() => handleClick('/ourwork')}>Nuestros trabajos</button>
             <button>Sobre nosotros</button>
             <button>Nuestro proceso</button>
-            <button>Contáctanos</button>
+            <button className={activeLink === '/contact' ? 'font-bold' : ''} onClick={() => handleClick('/contact')}>Contáctanos</button>
         </div>
     </div>
   )
