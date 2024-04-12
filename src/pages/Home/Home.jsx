77ea8bb82ad6eb88a868890fc458d490ddaea5d8
Aaron <ravycrow@gmail.com>
2024-04-12 12:01:49 +0100

    welcome2.0

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,38 +3,35 @@ import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import './Home.css'
 const Home = () => {
-  const [currentDiv, setCurrentDiv] = useState(1);
  
-  const handleClick = (divId) => {
-    const div = document.getElementById(divId);
-    div.scrollIntoView({ behavior: "smooth" });
-  };
-
   return (
-    <main >
-      <div id='containerHome' className="">
+    <main className='h-full w-full' >
+      <div id='containerHome' className="flex flex-col h-full overflow-y-scroll relative">
         <div className='fixed'>
         <button className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleClick('home1')}><KeyboardArrowUpIcon/></button>
         </div>
 
-          <div id='home1' className=" bg-blue-200 flex flex-col justify-center items-center ">
-            <p>Contenido del Home 1</p>
-            <button className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleClick('home2')}><KeyboardArrowDownIcon/></button>
+          <div id='home1' className=" bg-blue-200 flex flex-col items-center ">
+            <div className=''>
+            <a href='#home2'><KeyboardArrowDownIcon/></a>
+            </div>
           </div>
 
 
           <div id='home2' className=" bg-green-200 flex flex-col justify-center items-center">
-            <p>Contenido del Home 2</p>
-            <button className="ml-4 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleClick('home1')}><KeyboardArrowUpIcon/></button>
-            <button className="ml-4 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleClick('home3')}><KeyboardArrowDownIcon/></button>
+            <div>
+              <h1>Nuestros Servicios</h1>
+            </div>
+            <a href='#home1'><KeyboardArrowUpIcon/></a>
+            <a href='#home3'><KeyboardArrowDownIcon/></a>
         </div>
 
 
           <div id='home3' className=" bg-yellow-200 flex flex-col justify-center items-center">
             <p>Contenido del Home 3</p>
-            <button className="ml-4 bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleClick('home2')}><KeyboardArrowUpIcon/></button>
+            <a href='#home2'><KeyboardArrowUpIcon/></a>
           </div>
-        </div>
+      </div>
     </main>
   );
 };
