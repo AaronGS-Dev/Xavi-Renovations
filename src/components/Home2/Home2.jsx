3ab35f44514528fe2072cf7b316ce855c2e8c885
Aaron <ravycrow@gmail.com>
2024-05-06 22:44:21 +0100

    welcome

--- src/components/Home2/Home2.jsx
+++ src/components/Home2/Home2.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import 'animate.css/animate.min.css'
 
 const Home2 = () => {
   return (
@@ -8,13 +9,13 @@ const Home2 = () => {
          <div className='top-0 flex justify-center'>
             <a href='#home1'><KeyboardArrowUpIcon/></a>
           </div>
-          <div id='service' className='bg-white flex flex-col text-center w-4/5	h-full rounded ml-auto'>
+          <div id='service' className='bg-white flex flex-col text-center w-4/5	h-full rounded ml-auto animate__animated animate__fadeIn'>
             <div>
               <h1 className='p-6'>Nuestros Servicios</h1>
             </div>
             <div className="flex justify-center items-center">
-              <img src="\public\image\maintenance.png" className='w-1/6 h-auto' alt="" />
-              <p>Ofrecemos los siguientes servicios</p>
+              <img src="\public\image\maintenance.png" className='w-1/6 h-auto ' alt="" />
+              <p className=''>Ofrecemos los siguientes servicios</p>
             </div>
             <div className="flex justify-center items-center">
               <p>Grandes obras de renovación</p>
