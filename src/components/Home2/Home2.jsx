ae664b530f6aa71091ae036dc7ecab4d2c5c6aa9
Aaron <ravycrow@gmail.com>
2024-05-08 10:22:18 +0100

    Welcome avanzado

--- src/components/Home2/Home2.jsx
+++ src/components/Home2/Home2.jsx
@@ -1,15 +1,8 @@
 import React from 'react'
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import 'animate.css/animate.min.css'
-
 const Home2 = () => {
   return (
     <main>
-         <div className='top-0 flex justify-center'>
-            <a href='#home1'><KeyboardArrowUpIcon/></a>
-          </div>
-          <div id='service' className='bg-white flex flex-col text-center w-4/5	h-full rounded ml-auto animate__animated animate__fadeIn'>
+          <div id='service' className='bg-white  bg-opacity-50 flex flex-col text-center w-4/5	h-full rounded ml-auto'>
             <div>
               <h1 className='p-6'>Nuestros Servicios</h1>
             </div>
@@ -33,9 +26,6 @@ const Home2 = () => {
               <img src="\public\image\plumbing.png" className='w-1/6 h-auto' alt="" />
               <p>Fontaneria a fondo</p>
             </div>
-          </div>
-          <div className='bottom-0 flex justify-center'>
-              <a href='#home3' ><KeyboardArrowDownIcon/></a>
           </div>
     </main>
   )
