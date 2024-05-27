72281b35926bd50b681141141788233fc3f90e4b
Aaron <ravycrow@gmail.com>
2024-05-27 16:03:50 +0100

    slider home 1

--- src/components/Home1/Home1.jsx
+++ src/components/Home1/Home1.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import "./Home1.css"
 
 const Home1 = () => {
   return (
-    <div className='bottom-0'>
-    {/* <a href='#home2' className=''><KeyboardArrowDownIcon/></a> */}
+    <div id='slider' className=''>
+      <img src="https://res.cloudinary.com/dytjoh7oh/image/upload/v1715426799/XaviRenovaties/Reforma%202/IMG_1558-scaled_aunq5f.jpg" alt="" />
+      <img src="https://res.cloudinary.com/dytjoh7oh/image/upload/v1714072686/XaviRenovaties/Reforma%201/7fe95d10-7757-4d66-9e48-ca39df63c211_uzgoox.jpg" alt="" />
+      <img src="https://res.cloudinary.com/dytjoh7oh/image/upload/v1714072686/XaviRenovaties/Reforma%201/8b08ea9a-6bcf-4125-8961-d462ca9fedd4_c9lmoc.jpg" alt="" />
     </div>
   )
 }
