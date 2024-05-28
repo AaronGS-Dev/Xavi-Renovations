9128b65af41f35a7b38d662601ac2d1ba33329d5
Aaron <ravycrow@gmail.com>
2024-05-28 15:36:33 +0100

    todas las fotos incluidas

--- src/components/Home1/Home1.jsx
+++ src/components/Home1/Home1.jsx
@@ -6,9 +6,20 @@ const Home1 = () => {
     <div id='slider' className=''>
       <img src="https://res.cloudinary.com/dytjoh7oh/image/upload/v1715426799/XaviRenovaties/Reforma%202/IMG_1558-scaled_aunq5f.jpg" alt="" />
       <img src="https://res.cloudinary.com/dytjoh7oh/image/upload/v1714072686/XaviRenovaties/Reforma%201/7fe95d10-7757-4d66-9e48-ca39df63c211_uzgoox.jpg" alt="" />
-      <img src="https://res.cloudinary.com/dytjoh7oh/image/upload/v1714072686/XaviRenovaties/Reforma%201/8b08ea9a-6bcf-4125-8961-d462ca9fedd4_c9lmoc.jpg" alt="" />
-      <img src="https://res.cloudinary.com/dytjoh7oh/image/upload/v1714072685/XaviRenovaties/Reforma%201/f860caee-e8d1-46be-b0c2-07c31fedf0d5_y5o7pw.jpg" alt="" />
       <img src="https://res.cloudinary.com/dytjoh7oh/image/upload/v1714072689/XaviRenovaties/Reforma%201/8090eb18-535e-4d69-8a6a-ffc36e1de07e_yjvg3g.jpg" alt="" />
+      <img src="https://res.cloudinary.com/dytjoh7oh/image/upload/v1714072686/XaviRenovaties/Reforma%201/2f44efa1-46ba-42b8-b329-1dee609fe4a2_e4bdha.jpg" alt="" />
+      <img src="https://res.cloudinary.com/dytjoh7oh/image/upload/v1714072685/XaviRenovaties/Reforma%201/1ec909e5-8c34-461d-ac40-8fb2b7d382f7_faol2r.jpg" alt="" />
+      <img src="https://res.cloudinary.com/dytjoh7oh/image/upload/v1714072685/XaviRenovaties/Reforma%201/f860caee-e8d1-46be-b0c2-07c31fedf0d5_y5o7pw.jpg" alt="" />
+      <img src="https://res.cloudinary.com/dytjoh7oh/image/upload/v1714072686/XaviRenovaties/Reforma%201/8b08ea9a-6bcf-4125-8961-d462ca9fedd4_c9lmoc.jpg" alt="" />
+      <img src="https://res.cloudinary.com/dytjoh7oh/image/upload/v1714125221/XaviRenovaties/Reforma%201/4226ceb6-cb0f-42f3-8574-b3cb68654fc3_uhqbzt.jpg" alt="" />
+      <img src="https://res.cloudinary.com/dytjoh7oh/image/upload/v1714072686/XaviRenovaties/Reforma%201/4d336d47-1488-4958-9922-5d36f014a9e2_nktda1.jpg" alt="" />
+      <img src="https://res.cloudinary.com/dytjoh7oh/image/upload/v1714072685/XaviRenovaties/Reforma%201/c3c9e693-3668-4398-8c84-8314d795f94c_ymwauz.jpg" alt="" />
+      <img src="https://res.cloudinary.com/dytjoh7oh/image/upload/v1714072687/XaviRenovaties/Reforma%201/9f471187-af91-4841-b7ba-1dba2e5e0f00_fq7lk4.jpg" alt="" />
+      <img src="https://res.cloudinary.com/dytjoh7oh/image/upload/v1714072688/XaviRenovaties/Reforma%201/43a487d9-9dc1-4eab-8487-d133f11d2abd_ou1nep.jpg" alt="" />
+      <img src="https://res.cloudinary.com/dytjoh7oh/image/upload/v1714072657/XaviRenovaties/Reforma%201/4374115c-5d64-42cf-a02c-0b9dab584257_yidzmm.jpg" alt="" />
+      <img src="https://res.cloudinary.com/dytjoh7oh/image/upload/v1714072685/XaviRenovaties/Reforma%201/cd08b4b8-e5ec-40f3-b580-483d0a08e9f1_pwaeu7.jpg" alt="" />
+      <img src="https://res.cloudinary.com/dytjoh7oh/image/upload/v1714072684/XaviRenovaties/Reforma%201/c6f69d44-fad1-413d-bdea-4f22710b640a_r3vux4.jpg" alt="" />
+        
     </div>
   )
 }
