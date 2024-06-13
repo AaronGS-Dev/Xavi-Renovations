5aa14cad6dc2908db146c23146ebe45fc6b08b03
DomPar <djps94@outlook.com>
2024-06-13 11:16:35 +0100

    cosas

--- src/components/Home3/Home3.jsx
+++ src/components/Home3/Home3.jsx
@@ -4,33 +4,11 @@
   const Home3 = () => {
     const { t } = useTranslation();
     return (
-      <div className='flex flex-col justify-center'>
-      
-        <div id='containerHome3' className='grid grid-cols-2 gap-10 text-center'>
-          <div className='bg-white bg-opacity-80 rounded p-6 text-xl'>
-            <p>{t("home2.first")}</p>
-          </div>
-          <div className='bg-white bg-opacity-80 rounded p-6 text-xl'>
-            <p>{t("home2.second")}</p>
-          </div>
-          <div className='bg-white bg-opacity-80 rounded p-6 text-xl'>
-            <p>{t("home2.third")}</p>
-          </div>
-          <div className='bg-white bg-opacity-80 rounded p-6 text-xl'>
-            <p>{t("home2.fourth")}</p>
-          </div>
-          <div className='bg-white bg-opacity-80 rounded p-6 text-xl'>
-            <p>{t("home2.fifth")}</p>
-          </div>
-          <div className='bg-white bg-opacity-80 rounded p-6 text-xl'>
-            <p>{t("home2.sixth")}</p>
-          </div>
-          <div className='bg-white bg-opacity-80 rounded p-6 text-xl'>
-            <p>{t("home2.seventh")}</p>
-          </div>
-          <div className='bg-white bg-opacity-80 rounded p-6 text-xl'>
-            <p>{t("home2.eighth")}</p>
-          </div>
+      <div className='h-[100%] w-[100%] flex justify-center items-center'>
+        <div id='containerHome3' className='w-[90%] h-[90%] text-center bg-white bg-opacity-80'>
+          Hola
+        </div>
+        <div id='containerHome3' className='w-[90%] h-[90%] text-center bg-none'>
         </div>
       </div>
     );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -33,15 +33,14 @@ const Home = () => {
              <a href='#home3' ><KeyboardArrowDownIcon/></a>  
             </div>
             </div>
-            <div id='home3' className=' h-[100%] xl:h-[100%] w-[100%] xl:w-[100%] flex flex-col justify-center items-center'>
-              <a href='#home2'><KeyboardArrowUpIcon/></a>
-          <div className=' h-[90%] xl:h-[90%] w-[90%] xl:w-[90%] bg-[url("../image/Home3.jpg")] 
-          bg-contain bg-no-repeat bg-center xl:bg-cover xl:bg-center flex flex-col justify-center items-center'>
-            <Fade duration={1000} delay={500}>
+        <div id='home3' className=' h-[100%] xl:h-[100%] w-[100%] xl:w-[100%] flex flex-col justify-center items-center'>
+          <a href='#home2'><KeyboardArrowUpIcon/></a>
+          <div className=' h-[90%] xl:h-[90%] w-[90%] xl:w-[90%] bg-[url("../image/Home1.jpg")] 
+            bg-contain bg-no-repeat bg-center xl:bg-cover xl:bg-center'>
+            {/* <Fade duration={1000} delay={500}>            </Fade> */}
             <Home3/>
-            </Fade>
           </div>
-          </div>
+        </div>
       </div>
     </main> 
   );
