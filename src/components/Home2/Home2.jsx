e7e42776f609288001d98404c039d7166b729254
Aaron <ravycrow@gmail.com>
2024-06-13 11:28:20 +0100

    18commit en 20 min

--- src/components/Home2/Home2.jsx
+++ src/components/Home2/Home2.jsx
@@ -4,13 +4,13 @@ import { useTranslation } from 'react-i18next';
 const Home2 = () => {
   const { t } = useTranslation();
   return (
-    <main className='h-[100%] w-[100%]'>
+    <main className='h-[100%] w-[100%] flex justify-evenly items-center'>
 
-      <div className='' >
+      <div className='w-[40%] h-[80%] text-center bg-none'>
         
       </div>
 
-      <div className=''>
+      <div className='w-[40%] h-[80%] text-center bg-white bg-opacity-80'>
 
       </div>
 
