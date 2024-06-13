ebf9ddcd58d19fb45b6eb34344392f1d41a959dc
Aaron <ravycrow@gmail.com>
2024-06-13 13:39:20 +0100

    asiksdja

--- src/components/Home2/Home2.jsx
+++ src/components/Home2/Home2.jsx
@@ -17,13 +17,13 @@ const Home2 = () => {
         
       </div>
 
-      <div id='containerHome2' className='w-[40%] h-[80%] bg-white bg-opacity-80 flex flex-col items-center gap-4 p-8 text-xl'>
+      <div id='containerHome2' className='w-[40%] h-[80%] bg-white bg-opacity-80 flex flex-col items-center gap-4 p-16 text-xl'>
         <h1 className=''>Descubre nuestras obras y diseños.</h1>
          <p>En cada obra y remodelación, creamos ambientes que reflejan tu estilo y personalidad. Desde diseño de interiores hasta renovaciones completas,
           nuestro equipo se dedica a hacer realidad tus ideas.
           Descubre cómo nuestros proyectos destacan por su innovación y funcionalidad. Con un enfoque único en cada detalle, logramos espacios que inspiran y transforman.
           Explora nuestros trabajos y déjate llevar por la creatividad.</p> 
-        <div className='flex flex-col gap-16 '>
+        <div className='flex gap-16'>
         <button  onClick={() => handleClick('/ourwork')} className="button-57" role="button"><span class="text">{t('header.works')}</span><span>{t('header.works')}</span></button>
 
         <button  onClick={() => handleClick('/design')} className="button-57" role="button"><span class="text">{t('header.design')}</span><span>{t('header.design')}</span></button>
