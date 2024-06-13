52cddf16063f1a901e8e6eb2d60d1181c5f5d1cb
Aaron <ravycrow@gmail.com>
2024-06-13 15:03:47 +0100

    hom1

--- src/components/Home1/Home1.jsx
+++ src/components/Home1/Home1.jsx
@@ -15,10 +15,10 @@ const Home1 = () => {
 
   return (
     <main className='h-[100%] w-[100%] flex justify-evenly items-center'>
-      <div className='w-[40%] h-[80%] bg-slate-200 bg-opacity-80 flex flex-col items-center gap-4 p-16 text-xl'>
-      <h1>Xavi Renovaties-General Renovations</h1>
+      <div className='w-[40%] h-[80%] flex flex-col items-center gap-4 p-16 text-xl'>
+      <h1>Xavi Renovaties</h1>
         <p> XaviRenovaties es una empresa consolidada en el sector de la reforma integral desde hace más de 15 años. Especializada y dedicada a la reforma de viviendas de todo tipo, proyectos de interiores, locales, chalets, naves, garajes y comunidades de propietarios.</p>
-        <p>¿Listo para comenzar tu próxima obra? Contáctanos y hagamos realidad tu visión hoy mismo.</p>
+        <button onClick={() => handleClick('/about')} class="button-57" role="button"><span class="text">{t('header.about')}</span><span>{t('header.about')}</span></button>
       </div>
    
       <div>
