7d78ae8ba403fe88e624cc6896962ff11e7a5dee
Aaron <ravycrow@gmail.com>
2024-06-13 10:29:09 +0100

    commit para luego borrarlo -.-

--- src/components/Home1/Home1.jsx
+++ src/components/Home1/Home1.jsx
@@ -16,16 +16,18 @@ const Home1 = () => {
   return (
     <main className='flex justify-evenly mt-10'>
       <div>
-        <div>
-        <h1>Xavi Renovations</h1>
+        <div className='flex justify-center'>
+        <h1>Xavi Renovaties</h1>
         </div>
-        <div>
-          <p>Descubre nuestras obras más destacadas y la calidad que nos define.</p>
-          <button onClick={() => handleClick('/ourwork')}>{t('header.works')}</button>
-        </div>
-        <div>
-          <p>Explora los proyectos que hemos llevado a cabo con pasión y dedicación.</p>
-          <button onClick={() => handleClick('/ourwork')}></button>
+        <div className='h-[90%] xl:h-[90%] w-[90%] xl:w-[90%] flex flex-col gap-10 '>
+          <div className='mt-10'>
+            <p>Descubre nuestras obras más destacadas y la calidad que nos define.</p>
+            <button id='button' onClick={() => handleClick('/ourwork')}>{t('header.works')}</button>
+          </div>
+          <div>
+            <p>Explora los proyectos que hemos llevado a cabo con pasión y dedicación.</p>
+            <button id='button' onClick={() => handleClick('/design')}>{t('header.design')}</button>
+          </div>
         </div>
       </div>
    
